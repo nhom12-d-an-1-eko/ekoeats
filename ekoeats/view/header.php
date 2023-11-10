bd008a178d607f73abf8354b127dbe395990d31e
Phú <125366020+kaishenlong@users.noreply.github.com>
2023-11-11 01:16:45 +0700

    Add files via upload

---
+++ ekoeats/view/header.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>EKO EATS</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
+        integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css" />
+    <link rel="icon" href="img/ekoeats.jpg">
+    <link rel="stylesheet" href="../asset/css/style.css">
+</head>
+
+<body>
+    <div class="container">
+
+        <!-- Start header -->
+        <div class="header">
+            <div class="header-left">
+                <ul>
+                    <li><a href="index.html">
+                            <img width="100%"
+                                src="../img/ekoeats.jpg" alt="">
+                        </a>
+                    </li>
+                    <li><a href="menu.html">THỰC ĐƠN</a></li>
+                    <li><a href="about-us.html">VỀ CHÚNG TÔI</a></li>
+                    <li><a href="contact.html">LIÊN HỆ CHÚNG TÔI</a></li>
+                </ul>
+            </div>
+            <div class="header-right">
+                <ul>
+                    <li><a href="login.html"><i class="fa-solid fa-user icon fa-lg"></i></a></li>
+                    <li><a href="cart.html"><i class="fa-solid fa-cart-shopping fa-lg"></i></a></li>
+                    <li><i class="fa-solid fa-bars fa-lg"></i></li>
+                </ul>
+            </div>
+        </div>
+        <!-- End header -->
