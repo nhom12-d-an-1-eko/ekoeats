9694eace2a64b75dc8ba4c8a104393efd1570303
phu <phulqph25012@fpt.edu.vn>
2023-11-11 16:04:46 +0700

    tao file update danh muc them case sua xoa update file index

--- ekoeats/admin/index.php
+++ ekoeats/admin/index.php
@@ -15,6 +15,30 @@
             //         $listdanhmuc=loadall_danhmuc();
                     include "./danhmuc/list.php";
                     break;
+            
+            case "xoadm":
+                if(isset($_GET['id'])&&($_GET['id']>0)){
+                    delete_danhmuc($_GET['id']);
+                }
+                $listdanhmuc=loadall_danhmuc();
+                include "./danhmuc/list.php";
+                break;
+            case "suadm":
+                if(isset($_GET['id'])&&($_GET['id']>0)){
+                    $dm=loadone_danhmuc($_GET['id']);
+                }
+                include "./danhmuc/update.php";
+                break;
+            case "updatedm":
+                if(isset($_POST['capnhat'])&& ($_POST['capnhat'])){
+                    $tenloai=$_POST['tenloai'];
+                    $id=$_POST['id'];
+                    update_danhmuc($id,$tenloai); 
+                    $thongbao="cập nhật thành công";
+                }
+                $listdanhmuc=loadall_danhmuc();
+                include "./danhmuc/list.php";
+                break;
             }
         }else{
             include "home.php";
