d01797ea0d28c1cd8e21419a165466bd232be10c
Pham Thi Thu Lan <lan@gmail.com>
2023-11-11 23:09:40 +0700

    Cap nhat index.php

--- ekoeats/admin/index.php
+++ ekoeats/admin/index.php
@@ -1,6 +1,7 @@
 <?php
     include("../model/pdo.php");
     include("../model/danhmuc.php");
+    include("../model/sanpham.php");
     include("header.php");
     if(isset($_GET['act'])&&($_GET['act']!="")){
         $act=$_GET['act'];
@@ -41,7 +42,83 @@
                 $listdm=loadall_dm();
                 include "./danhmuc/list.php";
                 break;
+            
+            //      CONTROLER SẢN PHẨM
+        case "addsp":
+            //kiểm tra xem người dùng có click vào nút add hay không
+            if(isset($_POST['addsp'])){
+                $id=$_POST['id'];
+                $iddm=$_POST['iddm'];
+                $tensp= $_POST['tensp'];
+                $giasp= $_POST['giasp'];
+                $mota= $_POST['mota'];
+                $hinh= $_FILES['hinh']['name'];
+                $soluong= $_POST['soluong'];
+                $target_dir= "../upload/";
+                $target_file= $target_dir . basename($_FILES["hinh"]["name"]);
+                if (move_uploaded_file($_FILES["hinh"]["tmp_name"], $target_file)) {
+                    //echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
+                } else {
+                   // echo "Sorry, there was an error uploading your file.";
+                }
+                insert_sp($tensp, $giasp,$hinh,$mota,$iddm,$soluong);
+                $thongbao="Thêm thành công";
             }
+            $listdm=loadall_dm();
+            include "sanpham/add.php";
+            break;
+        case 'listsp':
+            if(isset($_POST['listok'])&&($_POST['listok'])){
+                $kyw= $_POST['kyw'];
+                $iddm= $_POST['iddm'];
+            } else{
+                $kyw= '';
+                $iddm =0;
+            }
+            $listdm=loadall_dm();
+            $listsp = loadall_sp($kyw,$iddm);
+            include "sanpham/list.php";
+            break;
+        case 'xoasp':
+            if (isset($_GET['id'])&& ($_GET['id']>0)){
+                delete_sp($_GET['id']);
+            }
+            $listsp = loadall_sp("",0);
+            include "sanpham/list.php";
+            break;
+        case 'suasp':
+            if (isset($_GET['id'])&& ($_GET['id']>0)){
+                $sanpham=loadone_sp($_GET['id']);
+            }
+            $listdm=loadall_dm();
+            include "sanpham/update.php";
+            break;
+        case 'updatesp':
+
+            if(isset($_POST['capnhat']) && ($_POST['capnhat'])){
+                $iddm=$_POST['iddm'];
+                $id=$_GET['id'];
+                $tensp= $_POST['tensp'];
+                $giasp= $_POST['giasp'];
+                $mota= $_POST['mota'];
+                $hinh= $_FILES['hinh']['name'];
+                $soluong= $_POST['soluong'];
+                $target_dir= "../upload/";
+                $target_file= $target_dir . basename($_FILES["hinh"]["name"]);
+                if (move_uploaded_file($_FILES["hinh"]["tmp_name"], $target_file)) {
+                    //echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
+                } else {
+                    // echo "Sorry, there was an error uploading your file.";
+                }
+                update_sp($id,$iddm,$tensp,$giasp,$mota,$hinh,$soluong);
+                $thongbao="Cập nhật thành công";
+            }
+            $listdm=loadall_dm();
+            $listsp =loadall_sp();
+            include "sanpham/list.php";
+            break;
+        }
+        
         }else{
             include "home.php";
         }
