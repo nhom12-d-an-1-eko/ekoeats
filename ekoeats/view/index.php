9f793b7fa99e061d2c600ae099a732474917048d
phu <phulqph25012@fpt.edu.vn>
2023-11-13 17:57:29 +0700

    them act sanphamct

--- ekoeats/view/index.php
+++ ekoeats/view/index.php
@@ -4,7 +4,7 @@
     include "../model/danhmuc.php";
     include "header.php";
     include "../global.php";
-    $listsp = loadall_sp($kyw,$iddm);
+    $listsp = loadall_sp();
     $listdm = loadall_dm();
     if(isset($_GET['act'])&&($_GET['act']!="")){
       $act=$_GET['act'];
@@ -24,6 +24,15 @@
               $name= load_ten_dm($iddm);
               include "home.php";
               break;
+              case "sanphamct":
+                if(isset($_GET['idsp']) && $_GET['idsp'] > 0){
+                    $listsp = loadone_sanpham($_GET['idsp']);
+                    $listsp = load_sanpham_cungloai($_GET['idsp'], $sanpham['iddm']);
+                    include "view/chitietsanpham.php";
+                }else{  
+                    include "view/home.php";            
+                }
+                break;
             }
          }else{
             include "home.php";
