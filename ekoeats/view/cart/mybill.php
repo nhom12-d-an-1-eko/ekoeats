efb631de02f8414cd5919326b51f1e560d7c7259
Pham Thi Thu Lan <lan@gmail.com>
2023-11-21 21:24:44 +0700

    css

--- ekoeats/view/cart/mybill.php
+++ ekoeats/view/cart/mybill.php
@@ -42,55 +42,7 @@
         </table>
        </div>
     </div>
-    <style>
-    .cart{
-    margin: 150px auto 50px;
-    width: 1200px;
-    height: 100%;
-}
-.cart h1{
-    margin: 50px 0;
-}
-table, th, td {
-    border: 1px solid rgb(252, 45, 45);
-    border-collapse: collapse;
-    padding: 10px 20px;
    
-   
-  }
-  table th {
-    background-color: rgb(252, 45, 45);
-    color: aliceblue;
-  }
-  .form-cart{
-    display: grid;
-    grid-template-columns: 10fr 1fr;}
-   button {
-        width: 32px;
-        height: 32px;
-        background-color: #e36872dd;
-        border: none;
-        border-radius: 5px;
-        font-size: 20px;
-    }
-    input{
-            width: 32px;
-            height: 32px;
-            background-color: #d3737bdd;
-            border: none;
-            border-radius: 5px;
-            font-size: 15px;
-            padding: 8px;
-           
-        }
-    
-
-  </style>
-
-</div>
-</div>
-
-</style>
 
 </div>
 </div>
@@ -121,12 +73,14 @@ table, th, td {
         background-color: rgb(252, 45, 45);
         color: aliceblue;
     }
-
+    table {
+        height: 100%;
+        margin-bottom: 20px;
+    }
     .form-cart {
         display: grid;
         grid-template-columns: 10fr 1fr;
-        height: 80%;
-        
+       
         
     }
     button {
