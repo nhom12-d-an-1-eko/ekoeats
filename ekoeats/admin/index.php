d13791f3d96ee21d3f84475fbfef74b0fcbbada7
phu <phulqph25012@fpt.edu.vn>
2023-11-23 02:24:33 +0700

    them chi tiet don hang

--- ekoeats/admin/index.php
+++ ekoeats/admin/index.php
@@ -152,11 +152,12 @@ include "header.php";
             case "xoabl" : 
                 include "xoabl.php";
                 break;
+                ///////////////////
             case "dsdh" :   
-                if (isset($_POST['kyw'])) {
-                    $kyw=$_POST['kyw'];
-                }else{
-                    $kyw="";
+                if(isset($_POST['listok'])&&($_POST['listok'])){
+                    $kyw= $_POST['kyw'];
+                } else{
+                    $kyw= '';
                 }
                 $listbill=loadall_bill($kyw,0);
                 include "donhang/dsdh.php";
@@ -166,6 +167,20 @@ include "header.php";
                     $bill=loadone_dh($_GET['id']);
                 } 
                 include "donhang/suadh.php";
+                break;
+            case "chitietdh":  
+                // if(isset($_POST['chitietdh'])&&($_POST['chitietdh'])){
+                //     $idbill= $_POST['idbill'];
+                //     $listct = loadall_sp($iddm);
+                // } else{
+                //     $iddm =0;
+                // }
+                if(isset($_GET['id'])&&($_GET['id']>0)){  
+                    $bill=loadone_dh($_GET['id']);
+                } 
+                $bill=loadone_dh($_GET['id']);
+                $listct = loadct_dh($_GET['id']); 
+                include "donhang/billchitiet.php";
                 break;
             case "themdh" :   
                 include "donhang/themdh.php";
